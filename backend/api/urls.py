b399bd7f76c2b35533cd44a283340ab8891e1802
Sasha-Parshintcev <aleksandr-par-par@yandex.ru>
2024-07-16 18:26:08 +0700

    api urls categories, ingredients, dishes

---
+++ backend/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path, re_path
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoryViewSet, IngredientViewSet, DishViewSet
+
+
+api_v1 = DefaultRouter()
+api_v1.register('categories', CategoryViewSet, basename='categories')
+api_v1.register('ingredients', IngredientViewSet, basename='ingredients')
+api_v1.register('dishes', DishViewSet, basename='dishes')
+
+urlpatterns = [
+    path('', include(api_v1.urls)),
+    re_path('auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls')),
+]
