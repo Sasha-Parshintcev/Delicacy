3f1aa68aed6a6198264cc1a4d2c311c6108305b4
Sasha-Parshintcev <aleksandr-par-par@yandex.ru>
2024-07-20 19:33:26 +0700

    fix

--- README.md
+++ README.md
@@ -8,3 +8,8 @@
 4. python manage.py migrate
 5. python manage.py createsuperuser
 6. python manage.py runserver
+7. npm run dev (frontend)
+<!-- npm init --yes
+npm install --save-dev eslint eslint-loader
+npm install --save eslint eslint-loader angular angular-resource angular-route json-loader mustache-loader lodash
+pip install django-cors-headers -->
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'food.apps.FoodConfig',
     'api.apps.ApiConfig',
     'rest_framework',
+    'corsheaders',
     'rest_framework.authtoken',
     'djoser',
 ]
@@ -39,11 +40,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'config.urls'
