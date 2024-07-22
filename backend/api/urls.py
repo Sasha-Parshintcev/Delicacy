cff84899ed956a852aeea4caef1885b62f95bcc0
Sasha-Parshintcev <aleksandr-par-par@yandex.ru>
2024-07-22 15:42:51 +0700

    urls user

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, IngredientViewSet, DishViewSet
+from .views import (
+    CategoryViewSet,
+    IngredientViewSet,
+    DishViewSet,
+    UserViewSet
+)
 
 
 api_v1 = DefaultRouter()
+api_v1.register('users', UserViewSet, basename='users')
 api_v1.register('categories', CategoryViewSet, basename='categories')
 api_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 api_v1.register('dishes', DishViewSet, basename='dishes')
