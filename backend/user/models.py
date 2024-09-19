eb357f6b65911b18166aa78f6f57428459b3e9ee
Sasha-Parshintcev <aleksandr-par-par@yandex.com>
2024-09-19 16:07:37 +0700

    user model

--- backend/user/models.py
+++ backend/user/models.py
@@ -9,57 +9,23 @@ MAX_LENGTH_MODEL = 150
 
 class User(AbstractUser):
     """
-    Модель User расширяет стандартную модель AbstractUser из Django
+    Модель User расширяет стандартную модель AbstractUser из Django,
+    добавляя возможность добавления аватара пользователя
     и предназначена для хранения информации о пользователях в системе.
     Регистрация пользователей осуществляется с помощью email,
     что позволяет улучшить безопасность
     и облегчить процесс восстановления пароля.
     """
-    email = models.EmailField(
+     email = models.EmailField(
         'email-адрес',
-        max_length=MAX_LENGTH_MODEL,
+        max_length=254,
         unique=True,
-    )
-    username = models.CharField(
-        'Логин',
-        max_length=MAX_LENGTH_MODEL,
-        validators=[UnicodeUsernameValidator()],
-        unique=True,
-        error_messages={
-            'unique': 'Пользователь с таким username уже существует.',
-        }
-    )
-    first_name = models.CharField(
-        'Имя',
-        max_length=MAX_LENGTH_MODEL
-    )
-    last_name = models.CharField(
-        'Фамилия',
-        max_length=MAX_LENGTH_MODEL
-    )
-    password = models.CharField(
-        'Пароль',
-        max_length=MAX_LENGTH_MODEL,
     )
     avatar = models.ImageField(
         'аватар',
         upload_to='users/',
         blank=True,
         default=None
-    )
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name=('groups'),
-        blank=True,
-        help_text =('Группы, к которым принадлежит этот пользователь'),
-        related_name='custom_user_group'
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name=('user permissions'),
-        blank=True,
-        help_text=('Особые разрешения для этого пользователя'),
-        related_name='custom_user_permissions'
     )
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
